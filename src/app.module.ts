5766fd5a22988e2d3d6dde4142790bb62335d21d
Stanislav <stanislaworonin@yandex.ru>
2023-10-22 17:06:15 +0300

    FIX: config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { DbModule } from "./db.firebird/db.module";
 import { OrderModule } from "./features/order/order.module";
 import { AuthModule } from "./features/authorization/auth.module";
 import { IsValidDateConstraint } from "./common/decorators/isValidDate.decorator";
+import { join } from "path";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [DbModule, OrderModule, AuthModule],
+  imports: [
+    DbModule,
+    OrderModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: join(process.cwd(), ".env"),
+    }),
+  ],
   controllers: [],
   providers: [IsValidDateConstraint],
 })
